9df1219fa4342cc1bf0d9a4efbd03bc3e788efbb
nsavagejvm <you@example.com>
2018-06-01 16:12:46 +0300

    setup ok

--- src/main/java/learn/eth/EntryPoint.java
+++ src/main/java/learn/eth/EntryPoint.java
@@ -17,6 +17,14 @@ public class EntryPoint  {
 
 	public static void main(String[] args) {  run(EntryPoint.class, args); }
 
-
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+//
+//			boolean result = Files.deleteIfExists(Paths.get("./spring-shell.log"));
+//
+//			System.out.println("created test  "+result);
+		};
+	}
 
 }
